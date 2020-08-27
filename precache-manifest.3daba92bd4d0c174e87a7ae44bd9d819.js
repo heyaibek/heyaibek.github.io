859ec89bfa3e3cc3257647a50211685b1c1ae36d
Aibek Mazhitov <aimazh@pro.local>
2020-08-27 22:26:51 +0200

    initial commit

---
+++ precache-manifest.3daba92bd4d0c174e87a7ae44bd9d819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3cc55cbbcf29c9369931c9771e494cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33c8adea4ba482e88745",
+    "url": "/static/css/main.c749d73e.chunk.css"
+  },
+  {
+    "revision": "bd75a1892413cf1a9a0d",
+    "url": "/static/js/2.7223754e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7223754e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c8adea4ba482e88745",
+    "url": "/static/js/main.ca3fdc6c.chunk.js"
+  },
+  {
+    "revision": "e8d5ca278708057f20ce",
+    "url": "/static/js/runtime-main.1f3ca235.js"
+  }
+]);
