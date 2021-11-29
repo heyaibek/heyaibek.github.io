f7d993bb98e56be20adb2398234c9cc08c3e4d50
Aibek Mazhitov <amazhitov@gmail.com>
2021-11-29 20:10:17 +0100

    feat: added favicons + updated info

--- src/index.js
+++ src/index.js
@@ -21,9 +21,9 @@ const App = () => {
       <div className='header'>
         <h1 className='rainbow'>Aibek Mazhitov</h1>
         <div className='bio'>
-          <p>Hi there <span role='img' aria-label='hands-up'>👋</span>!</p>
-          <p>I'm a Software Engineer at <a href='https://www.flatex.com/'>flatexAG</a>.</p>
-          <p>I also created <a href='https://aimazh.github.io/chksndapp/'>ChkSnd</a>.</p>
+          <p>Hi there <span role='img' aria-label='hands-up'>👋</span></p>
+          <p>I'm a Software Engineer at <a href='https://www.flatex.com/'>flatexDEGIRO</a>.</p>
+          <p>I also created <a href='https://chksnd.app'>ChkSnd</a>.</p>
           <p>In my free time I produce electronic <a href='https://soundcloud.com/brakata'>music</a>.</p>
         </div>
       </div>
@@ -32,12 +32,14 @@ const App = () => {
         <a className='linkedin' href='https://www.linkedin.com/in/aimazh/'>
           <i className='fab fa-linkedin fa-2x' />
         </a>
+        <a className='instagram' href='https://instagram.com/brakatagoi'>
+          <i className='fab fa-instagram fa-2x' />
+        </a>
         <a className='github' href='https://github.com/aimazh'>
           <i className='fab fa-github fa-2x' />
         </a>
-        <a className='leetcode' href='https://leetcode.com/aimazh' style={{ backgroundImage: LeetCodeLogo }} />
-        <a className='spotify' href='https://open.spotify.com/user/217dpiqihjneujcuut5kl5wma?si=d7d861385e724b68'>
-          <i className='fab fa-spotify fa-2x' />
+        <a className='telegram' href='https://t.me/brakatamusic'>
+          <i className='fab fa-telegram fa-2x' />
         </a>
       </div>
 
@@ -50,14 +52,8 @@ const App = () => {
           <div className='project'>
             <img className='logo' src={ChkSndLogo} alt='logo' />
             <h2 className='name'>ChkSnd</h2>
-            <p className='description'>ChkSnd is a tool to create music<br />videos with a visual audio spectrum.<br /><br />Available for iOS and Android.</p>
-            <a className='website' href='https://chksnd.github.io/'>Website</a>
-          </div>
-          <div className='project'>
-            <img className='logo' src={FortylinesLogo} alt='logo' />
-            <h2 className='name'>Fortylines</h2>
-            <p className='description'>Fortylines is a team of<br />young and ambitious people,<br />who build and deliver high-quality<br />IT solutions to the world.</p>
-            <a className='website' href='https://fortylinesio.github.io/'>Website</a>
+            <p className='description'>ChkSnd is an app to create music<br />videos with a visual audio spectrum.<br /><br />Available for iOS and Android.</p>
+            <a className='website' href='https://chksnd.app'>Website</a>
           </div>
         </div>
       </div>
