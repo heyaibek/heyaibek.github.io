278eb08bf999f91cebf14be5172fc91ea963f903
Aibek <heyaibek@gmail.com>
2023-11-16 02:05:09 +0100

    add spacing for mobile surfaces

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,53 +1,58 @@
-import "@/styles/globals.css";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import '@/styles/globals.css';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function App({ Component, pageProps }) {
   const pathname = usePathname();
   return (
     <main>
-      <nav className="text-xs uppercase container mx-auto flex gap-4 font-bold py-4">
+      <nav className="text-xs uppercase container mx-auto flex gap-6 font-bold py-10">
         <div className="flex-1" />
         <Link
           className={`${
-            pathname === "/" ? "text-slate-900 dark:text-slate-100" : "text-slate-400"
+            pathname === '/' ? 'text-slate-900 dark:text-slate-100' : 'text-slate-400'
           }`}
-          href="/"
-        >
+          href="/">
           @heyaibek
         </Link>
         <Link
           className={`${
-            pathname.indexOf("/vibely") > -1 ? "text-slate-900 dark:text-slate-100" : "text-slate-400"
+            pathname.indexOf('/vibely') > -1
+              ? 'text-slate-900 dark:text-slate-100'
+              : 'text-slate-400'
           }`}
-          href="/vibely"
-        >
+          href="/vibely">
           Vibely
         </Link>
         <Link
           className={`${
-            pathname.indexOf("/brakata") > -1 ? "text-slate-900 dark:text-slate-100" : "text-slate-400"
+            pathname.indexOf('/brakata') > -1
+              ? 'text-slate-900 dark:text-slate-100'
+              : 'text-slate-400'
           }`}
-          href="/brakata"
-        >
+          href="/brakata">
           Brakata
         </Link>
         <Link
           className={`${
-            pathname.indexOf("/resume") > -1 ? "text-slate-900 dark:text-slate-100" : "text-slate-400"
+            pathname.indexOf('/resume') > -1
+              ? 'text-slate-900 dark:text-slate-100'
+              : 'text-slate-400'
           }`}
-          href="/resume"
-        >
+          href="/resume">
           Resume
         </Link>
         <div className="flex-1" />
       </nav>
-      <div className="flex gap-1 justify-center mb-3 rainbow">
+      <div className="flex gap-1 justify-center mb-10 rainbow">
         <span>~</span>
         <span>~</span>
         <span>~</span>
       </div>
-      <Component {...pageProps} />
+      <div className="px-10">
+        <Component {...pageProps} />
+      </div>
+      <div style={{ height: 50 }} />
     </main>
   );
 }
