4beb481d8b3b96471046bd7fb824125b094297b3
Aibek <heyaibek@gmail.com>
2024-04-09 02:15:10 +0200

    fix: update vibely open-source list

--- src/pages/vibely/open-source/index.tsx
+++ src/pages/vibely/open-source/index.tsx
@@ -14,108 +14,6 @@ export default function Home() {
         in portions of the ChkSnd application for iOS/Android. We thank the open source community
         for all of their contributions.
       </p>
-      <h3>CompactSlider</h3>
-      <code>
-        MIT License
-        <br />
-        <br />
-        Copyright (c) 2022 Alexey Bukhtin
-        <br />
-        <br />
-        Permission is hereby granted, free of charge, to any person obtaining a copy
-        of this software and associated documentation files (the &quot;Software&quot;), to deal
-        in the Software without restriction, including without limitation the rights
-        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions:
-        <br />
-        <br />
-        The above copyright notice and this permission notice shall be included in all
-        copies or substantial portions of the Software.
-        <br />
-        <br />
-        THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
-      </code>
-      <h3>AlertKit</h3>
-      <code>
-        MIT License
-        <br />
-        <br />
-        Copyright © 2021 Ivan Vorobei
-        <br />
-        <br />
-        Permission is hereby granted, free of charge, to any person obtaining a copy
-        of this software and associated documentation files (the &quot;Software&quot;), to deal
-        in the Software without restriction, including without limitation the rights
-        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions:
-        <br />
-        <br />
-        The above copyright notice and this permission notice shall be included in all
-        copies or substantial portions of the Software.
-        <br />
-        <br />
-        THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
-      </code>
-      <h3>PanModal</h3>
-      <code>
-        Copyright © 2018 Tiny Speck, Inc.
-        <br />
-        <br />
-        Permission is hereby granted, free of charge, to any person obtaining a copy of this
-        software and associated documentation files (the &quot;Software&quot;), to deal in the
-        Software without restriction, including without limitation the rights to use, copy, modify,
-        merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-        persons to whom the Software is furnished to do so, subject to the following conditions:
-        <br />
-        <br />
-        The above copyright notice and this permission notice shall be included in all copies or
-        substantial portions of the Software.
-        <br />
-        <br />
-        THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-        PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-        BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
-        OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-        DEALINGS IN THE SOFTWARE.
-      </code>
-      <h3>ReSwift</h3>
-      <code>
-        The MIT License (MIT) Copyright (c) 2016 ReSwift Contributors
-        <br />
-        <br />
-        Permission is hereby granted, free of charge, to any person obtaining a copy of this
-        software and associated documentation files (the &quot;Software&quot;), to deal in the
-        Software without restriction, including without limitation the rights to use, copy, modify,
-        merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-        persons to whom the Software is furnished to do so, subject to the following conditions:
-        <br />
-        <br />
-        The above copyright notice and this permission notice shall be included in all copies or
-        substantial portions of the Software.
-        <br />
-        <br />
-        THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-        PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-        BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
-        OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-        DEALINGS IN THE SOFTWARE.
-      </code>
       <h3>Unsplash Photo Picker for iOS</h3>
       <code>
         MIT License
@@ -142,12 +40,38 @@ export default function Home() {
         OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
         DEALINGS IN THE SOFTWARE.
       </code>
-      <h3>FontBlaster</h3>
+      <h3>DebouncedOnChange</h3>
+      <code>
+        MIT License
+        <br />
+        <br />
+        Copyright (c) 2022 Łukasz Rutkowski
+        <br />
+        <br />
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this
+        software and associated documentation files (the &quot;Software&quot;), to deal in the
+        Software without restriction, including without limitation the rights to use, copy, modify,
+        merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+        persons to whom the Software is furnished to do so, subject to the following conditions:
+        <br />
+        <br />
+        The above copyright notice and this permission notice shall be included in all copies or
+        substantial portions of the Software.
+        <br />
+        <br />
+        THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+        PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+        BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+        OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+        DEALINGS IN THE SOFTWARE.
+      </code>
+      <h3>SwiftyGif</h3>
       <code>
         The MIT License (MIT)
         <br />
         <br />
-        Copyright (c) 2015 Arthur Ariel Sabintsev
+        Copyright (c) 2016 Alexis Creuzot
         <br />
         <br />
         Permission is hereby granted, free of charge, to any person obtaining a copy of this
@@ -168,32 +92,12 @@ export default function Home() {
         OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
         DEALINGS IN THE SOFTWARE.
       </code>
-      <h3>TTRangeSlider</h3>
+      <h3>Spline iOS</h3>
       <code>
-        Copyright (c) 2015 Tom Thorpe &lt;code@tomthorpe.co.uk&gt;
+        MIT License
         <br />
         <br />
-        Permission is hereby granted, free of charge, to any person obtaining a copy of this
-        software and associated documentation files (the &quot;Software&quot;), to deal in the
-        Software without restriction, including without limitation the rights to use, copy, modify,
-        merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-        persons to whom the Software is furnished to do so, subject to the following conditions:
-        <br />
-        <br />
-        The above copyright notice and this permission notice shall be included in all copies or
-        substantial portions of the Software.
-        <br />
-        <br />
-        THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-        PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-        BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
-        OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-        DEALINGS IN THE SOFTWARE.
-      </code>
-      <h3>StepSlider</h3>
-      <code>
-        Copyright (c) 2015 spromicky &lt;spromicky@gmail.com&gt;
+        Copyright (c) 2022 Spline, Inc.
         <br />
         <br />
         Permission is hereby granted, free of charge, to any person obtaining a copy of this
